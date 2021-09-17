6c7ac0313cae5327399d86aad0bd5a82a219523c
harvey298 <marco77@live.co.uk>
2021-09-17 23:11:37 +0100

    fixed C bind creation errors

--- src/aur_ask.rs
+++ src/aur_ask.rs
@@ -3,14 +3,15 @@ pub mod aur_use {
     use serde::Deserialize;    
 
     #[derive(Debug, Deserialize)]
-    #[serde(rename_all = "camelCase")] 
+    #[serde(rename_all = "camelCase")]
+    #[repr(C)]
     pub struct AurResponse {
         pub version: i16,
         pub r#type: String,
         pub resultcount: i32,
     }
-    #[no_mangle]
-    pub async extern "C" fn does_pkg_exist(pkg_name: &str) -> Result<AurResponse, reqwest::Error> {
+    #[no_mangle] // &str
+    pub async extern "C" fn does_pkg_exist(pkg_name: &'static String) -> Result<AurResponse, reqwest::Error> {
         let resp: AurResponse = reqwest::get(pkg_name)
         .await?
         .json::<AurResponse>()
